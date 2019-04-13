3f2da26a4860983a5023363b999394c275e01c7c
dj46563 <tyler.james.page98@gmail.com>
2019-04-13 01:38:26 -0700

    Added some quick lights and a simple day night system

---
+++ Assets/Scripts/AutomaticLights.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutomaticLights : MonoBehaviour
+{
+    public SunRotation Sun;
+
+    public Renderer EmissionRenderer;
+    public Light Light;
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Light != null && EmissionRenderer != null)
+	    {
+	        if (Sun != null && Sun.isNight)
+	        {
+	            Light.enabled = true;
+                EmissionRenderer.material.EnableKeyword("_EMISSION");
+	        }
+	        else
+	        {
+	            Light.enabled = false;
+	            EmissionRenderer.material.DisableKeyword("_EMISSION");
+            }
+        }
+	}
+}
--- Assets/Scripts/SunRotation.cs
+++ Assets/Scripts/SunRotation.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SunRotation : MonoBehaviour {
+public class SunRotation : MonoBehaviour
+{
 
-	// Use this for initialization
+    public float AnglesPerSecond;
+    public float NightStartAngle;
+    public float NightEndAngle;
+
+    public bool isNight
+    {
+        get
+        {
+            // between 220 and 330
+            return transform.rotation.eulerAngles.x > NightStartAngle && 
+                   transform.rotation.eulerAngles.x < NightEndAngle;
+        }
+    }
+
+    // Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.rotation *= Quaternion.AngleAxis(10 * Time.deltaTime, Vector3.right);
+		transform.rotation *= Quaternion.AngleAxis(AnglesPerSecond * Time.deltaTime, Vector3.right);
+        Debug.Log(isNight);
 	}
 }
