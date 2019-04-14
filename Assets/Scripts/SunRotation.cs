c45b051f40c97cb8ce8c9df6132fbad241999b3c
dj46563 <tyler.james.page98@gmail.com>
2019-04-13 23:53:09 -0700

    Improved day night cyle by rotating the directional light and lower the intensity at night

--- Assets/Scripts/SunRotation.cs
+++ Assets/Scripts/SunRotation.cs
@@ -6,27 +6,33 @@ public class SunRotation : MonoBehaviour
 {
 
     public float AnglesPerSecond;
-    public float NightStartAngle;
-    public float NightEndAngle;
+
+    private Light light;
+    private float dot;
 
     public bool isNight
     {
         get
         {
             // between 220 and 330
-            return transform.rotation.eulerAngles.x > NightStartAngle && 
-                   transform.rotation.eulerAngles.x < NightEndAngle;
+            return dot > -0.2;
         }
     }
 
     // Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+	    light = GetComponent<Light>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		transform.rotation *= Quaternion.AngleAxis(AnglesPerSecond * Time.deltaTime, Vector3.right);
-        Debug.Log(isNight);
+	void Update ()
+	{
+        // Rotate the directional light on its x axis
+        transform.Rotate(Vector3.right, Time.deltaTime * AnglesPerSecond);
+	    dot = Vector3.Dot(transform.forward, Vector3.up);
+
+        // Lower the intensity to zero at night so there is no weird lighting on vertical objects
+	    light.intensity = Mathf.Clamp(Mathf.Sin(-dot * Mathf.PI / 2) + 0.5f, 0, 1);
 	}
 }
